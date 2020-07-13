1c1694f0b38a6a4d22ab66ba12da7c2a421bbfb3
Shiham M <shiham.mohamed@sg.ey.com>
2020-07-13 12:05:13 +0800

    initial commit

---
+++ src/main/java/ey/digital/j2eedemo/J2eedemoApplication.java
@@ -0,0 +1,13 @@
+package ey.digital.j2eedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2eedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ey/digital/j2eedemo/J2eedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ey.digital.j2eedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
