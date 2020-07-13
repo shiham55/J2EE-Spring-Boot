678ad165f49efead654c31d2f3bcc06241309a98
Shiham M <shiham.mohamed@sg.ey.com>
2020-07-13 14:50:04 +0800

    updated to java 11

--- src/main/java/ey/digital/j2eedemo/J2eedemoApplication.java
+++ src/main/java/ey/digital/j2eedemo/J2eedemoApplication.java
@@ -2,12 +2,20 @@ package ey.digital.j2eedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class J2eedemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(J2eedemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
