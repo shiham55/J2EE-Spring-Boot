fbe5e748e68fec86c5102a1164da9788973bb1f8
Shiham M <shiham.mohamed@sg.ey.com>
2020-07-18 15:36:42 +0800

    spring mvc added

---
+++ src/main/java/ey/digital/j2eedemo/HomeController.java
@@ -0,0 +1,20 @@
+package ey.digital.j2eedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")
+	public ModelAndView home(@RequestParam("name") String myName) {
+		ModelAndView mv = new ModelAndView();
+
+		mv.addObject("name", myName);
+		mv.setViewName("homepage");
+
+		return mv;
+	}
+}
